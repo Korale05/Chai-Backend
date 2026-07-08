76f993982cc70dceb707f755267df7d5e2d99a28
Korale05 <onkarkorale03@gmail.com>
2026-07-08 18:15:12 +0530

    Connect to Mongo Atlas

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import express from "express";
+import connectDB from "./db/db.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path : './.env'
+})
+
+connectDB()
